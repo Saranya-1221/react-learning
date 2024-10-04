121910b40809bd4036832df68f328145474d4045
Saranya Ragunath <sarragun@WKMZT778D6E9.global.publicisgroupe.net>
2024-10-04 23:33:15 +0530

    ep_1-inception

---
+++ app.js
@@ -0,0 +1,19 @@
+const element = React.createElement('h1',{id: 'heading'}, 'Hello world by React');
+
+const nestElement = React.createElement('div', {id: 'parent'},
+    [
+        React.createElement('div', {id: 'child1'},
+        [   
+            React.createElement('h1', {},'I am h1 tag'),
+            React.createElement('h2', {},'I am h2 tag')
+        ]),
+        React.createElement('div', {id: 'child2'},
+        [   
+            React.createElement('h1', {},'I am h1 tag'),
+            React.createElement('h2', {},'I am h2 tag')
+        ]),
+    ]
+)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(nestElement);
+root.render(element);
